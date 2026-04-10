f080030687f82cabe4c59295f64a0ac10d0c8b50
遠藤槙二 <shinjiendo1204@gmail.com>
2026-04-11 04:06:39 +0900

    dede

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,6 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
-};
-
-export default nextConfig;
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // これを true にすると最新すぎる最適化でスマホが死ぬことがあるので一旦 false
+  swcMinify: true, 
+}
+module.exports = nextConfig
