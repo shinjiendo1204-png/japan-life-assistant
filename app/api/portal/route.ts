30059791b596e9cc86867e1e8f81ac30091f9df3
遠藤槙二 <shinjiendo1204@gmail.com>
2026-04-11 12:18:17 +0900

    dedede

--- app/api/portal/route.ts
+++ app/api/portal/route.ts
@@ -3,7 +3,7 @@ import Stripe from 'stripe'
 import { createClient } from '@supabase/supabase-js'
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  apiVersion: '2024-06-20',
+  apiVersion: '2026-03-25.dahlia',
 })
 
 export async function POST(req: NextRequest) {
